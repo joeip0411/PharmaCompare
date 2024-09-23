4a48fe5fe459dc9f428facf791c31f3948d570dc
AzureAD\JoeIp <j.ip@bettube.com>
2024-09-23 19:29:52 +1000

    fix dbt project dir

--- dagster_project/project.py
+++ dagster_project/project.py
@@ -4,6 +4,6 @@ from dagster_dbt import DbtProject
 
 dbt_project_project = DbtProject(
     project_dir=Path(__file__).joinpath("..", "..", "dbt_project").resolve(),
-    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+    packaged_project_dir=Path(__file__).joinpath("..", "dbt-project").resolve(),
 )
 dbt_project_project.prepare_if_dev()
